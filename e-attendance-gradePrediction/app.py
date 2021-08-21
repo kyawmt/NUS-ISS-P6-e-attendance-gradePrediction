a686cfb26ff123d5a51543ee7d44a09a2076c406
jwong93 <jwong93@hotmail.com>
2021-08-21 16:01:21 +0800

    addport

--- e-attendance-gradePrediction/app.py
+++ e-attendance-gradePrediction/app.py
@@ -16,3 +16,7 @@ def predict():
         prediction = logReg.predict(query)
         print(prediction)
         return jsonify({'prediction': str(prediction)})
+
+
+if __name__ == '__main__':
+    app.run(port=5000, debug=True)
